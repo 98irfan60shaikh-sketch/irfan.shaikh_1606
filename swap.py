e0ae3718b86ca63b36958d225b8d622850441777
irfan shaikh <98irfan60shaikh@gmail.com>
2026-01-28 17:37:04 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,7 @@
+a=int(input("enter a first number:-"))
+b=int(input("enter a  second number :-"))
+print(f"before swapping a={a}and b={b}")
+a=a+b
+b=a-b
+a=a-b
+print(f"after swapping a={a} and b={b}")
