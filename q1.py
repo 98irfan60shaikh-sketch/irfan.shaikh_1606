edf55fe27e86a38cca4d63a2be3cb9e65ed4a8a5
irfan shaikh <98irfan60shaikh@gmail.com>
2026-02-04 19:16:16 +0530

    Delete q1.py

--- q1.py
+++
@@ -1,4 +0,0 @@
-radius = float(input("Enter the radius of the circle :- "))
-area=3.14*radius*radius
-circumference=2*3.14*radius
-print(f"Area of the circle is :- {area}, and Circumference of the circle is :- {circumference}")
