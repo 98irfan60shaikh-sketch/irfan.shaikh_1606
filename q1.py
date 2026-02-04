e3ad2928e82ef2434a417c3de842e6cd93297af1
irfan shaikh <98irfan60shaikh@gmail.com>
2026-02-04 19:03:24 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,4 @@
+radius = float(input("Enter the radius of the circle :- "))
+area=3.14*radius*radius
+circumference=2*3.14*radius
+print(f"Area of the circle is :- {area}, and Circumference of the circle is :- {circumference}")
