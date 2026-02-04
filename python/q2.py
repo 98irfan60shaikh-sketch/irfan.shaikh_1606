f2d0f5ed86fc4852eeda4cb4fc7b4146bc86d3d5
irfan shaikh <98irfan60shaikh@gmail.com>
2026-02-04 19:15:56 +0530

    Add files via upload

---
+++ python/q2.py
@@ -0,0 +1,7 @@
+n1=int(input("Enter a first number :-"))
+n2=int(input("Enter a second number :-"))
+sum=n1+n2
+divded=n1/n2
+sub=n1-n2
+multiply=n1*n2
+print(f"Sum of two number is :- {sum}, Divided value is :- {divded}, Subtracted value is :- {sub}, Multiplied value is :- {multiply}")
