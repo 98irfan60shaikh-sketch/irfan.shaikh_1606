72814e10ce23a22d3a54197048f1965aa360b11e
irfan shaikh <98irfan60shaikh@gmail.com>
2026-02-23 19:20:01 +0530

    Add files via upload

---
+++ python/Ex4AQ4B.py
@@ -0,0 +1,11 @@
+t1=(1,2,3,4,5)
+max1=t1[0]
+for i in t1:
+    if i>max1:
+        max1=i
+print("maximum value in tuple ",max1)
+min1=t1[0]
+for i in t1:
+    if i<min1:
+        min1=i
+print("minimum value in tuple ",min1)
